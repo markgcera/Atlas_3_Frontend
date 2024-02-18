e58ef6cd4266d2676964c020f5425f629591a818
brannn99 <bransometan@gmail.com>
2024-02-18 22:52:00 +0800

    TAS-2-login-form-containers-edited

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import './assets/css/index.css'
+import WebLoginContainer from './components/WebLoginContainer.tsx';
+//import './assets/css/index.css'
 
 const queryClient = new QueryClient()
 
@@ -21,7 +22,7 @@ enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <WebLoginContainer/>
         <ReactQueryDevtools/>
       </QueryClientProvider>
     </React.StrictMode>,
