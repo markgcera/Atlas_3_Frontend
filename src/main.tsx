f9a4ea48b1b07eb4eb45820aea3ca838da59a39c
Lu Xinming <xinmingsm11@gmail.com>
2024-03-16 12:16:05 +0800

    get rid of empty line and import css reset

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-//import './assets/css/index.css'
+import './assets/css/reset.css'
 
 const queryClient = new QueryClient()
 
@@ -18,7 +18,6 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
-
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
