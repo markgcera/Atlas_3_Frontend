0389851f68a6738727b4fec5e130d39239197973
Lu Xinming <xinmingsm11@gmail.com>
2024-03-16 12:15:08 +0800

    render everything in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/img/react.svg'
-import viteLogo from '/vite.svg'
-import {useQuery} from "@tanstack/react-query";
-import resolveURL from "./api/fetch.ts";
-import './assets/css/App.css'
+import WebLoginContainer from './components/authentication/WebLoginContainer.tsx';
+import MobileLoginContainer from "./components/authentication/MobileLoginContainer.tsx";
+import { useWindowSize } from "@uidotdev/usehooks";
+import { BREAKPOINTS } from "./components/constants.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const { isPending, data } = useQuery({
-    queryKey: ['resourceData'],
-    queryFn: () => fetch(resolveURL('/resource')).then((res) => res.json())
-  })
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{isPending ? "Loading..." : data.result}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const size = useWindowSize();
+  return size.width! <= BREAKPOINTS.MD ? <MobileLoginContainer/> : <WebLoginContainer/>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import WebLoginContainer from './components/WebLoginContainer.tsx';
 //import './assets/css/index.css'
 
 const queryClient = new QueryClient()
@@ -19,10 +18,11 @@ async function enableMocking() {
 }
 
 enableMocking().then(() => {
+
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
       <QueryClientProvider client={queryClient}>
-        <WebLoginContainer/>
+        <App/>
         <ReactQueryDevtools/>
       </QueryClientProvider>
     </React.StrictMode>,
