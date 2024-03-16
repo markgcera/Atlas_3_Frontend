a7f7ac12fc5f6b263dca0b4580b536775a49b84b
Lu Xinming <xinmingsm11@gmail.com>
2024-03-16 14:51:57 +0800

    removed code from experimentation

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,23 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import { QueryClient } from "@tanstack/react-query";
 import TestPage from "./shared/TestPage.tsx";
-// import { loader } from "./shared/loader.tsx";
-
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 10,
-    },
-  },
-})
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/test",
     Component: TestPage,
-    // loader: loader(queryClient)
   }
 ])
 
--- src/routes/shared/loader.tsx
+++
@@ -1,7 +0,0 @@
-import resolveURL from "@/api/fetch.ts";
-
-
-export const getResourceOptions = {
-  queryKey: ['resourceData'],
-  queryFn: () => fetch(resolveURL('/resource')).then((res) => res.json())
-}
