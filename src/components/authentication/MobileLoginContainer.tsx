f3c6221b17a131d4625ab3741f41f36f2faa8a88
Lu Xinming <xinmingsm11@gmail.com>
2024-03-16 12:14:16 +0800

    added namespace and changed to css modules

---
+++ src/components/authentication/MobileLoginContainer.tsx
@@ -0,0 +1,13 @@
+import styles from '../../assets/css/authentication/MobileLoginContainer.module.css'; // Import mobile-specific CSS
+
+const MobileLoginContainer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginForm}>
+        {/* Your login form goes here */}
+      </div>
+    </div>
+  );
+}
+
+export default MobileLoginContainer;
