ab74ca2f26a363546035643ece5a3ac230d186ad
Lu Xinming <xinmingsm11@gmail.com>
2024-03-19 18:13:20 +0800

    removed unnecessary library

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,10 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
